b2ce555c8928bde41f8ccc394fe5df4e19641a04
MohiniSarwade <67684056+MohiniSarwade@users.noreply.github.com>
2020-10-09 23:46:04 +0530

    Update MaximumNumberOfWaysToMakeCoinChange.cpp

--- MaximumNumberOfWaysToMakeCoinChange.cpp
+++ MaximumNumberOfWaysToMakeCoinChange.cpp
@@ -16,3 +16,32 @@ int count( int S[], int m, int n )
 
 	return table[n]; 
 } 
+/////////////////////////////////////////////////////////////////////
+long long int count( int S[], int m, int n )
+    {
+       
+        //code here.
+       long long int tab[m+1][n+1];
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+                tab[i][j]=0;
+        }
+        for(long long int  i=0;i<=m;i++)
+            tab[i][0]=1;
+        for(long long int i=0;i<=n;i++)
+            tab[0][i]=0;
+         for(long long int i=1;i<=m;i++)
+        {
+            for(long long int j=1;j<=n;j++)
+            {
+                if(j>=S[i-1])
+                    tab[i][j]=tab[i-1][j]+tab[i][j-S[i-1]];
+                else
+                    tab[i][j]=tab[i-1][j];
+                    
+            }
+        }
+        
+        return tab[m][n];
+    }
